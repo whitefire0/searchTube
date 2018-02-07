dc75ea4ab5422d8b72caee2bf44f920e1e9c27fd
whitefire0 <rickhallett@live.co.ukk>
2018-02-07 13:12:22 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const API_KEY = 'AIzaSyBZ7GYfGfNVdsuWDL2y7o_rNnJkAe2 - av0';
-
-//Create a new component that produces HTML
-const App  = () => {
-  return <div>Hello World!</div>
-}
-
-
-//Inject generated HTML and inject it into the DOM
-ReactDOM.render(
-  <App />, 
-  document.querySelector('.container')
-);
